24d29cd193c015e1ffedc1cf5e752e58eb76127d
Roger Rajaratnam <roger@sourcier.uk>
2020-03-20 22:45:40 +0000

    BinaryGap solution

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Codility
 
 Solutions for codility lessons
+
+BinaryGap [ [Problem](https://app.codility.com/programmers/lessons/1-iterations/binary_gap/) ][ [Solution](https://app.codility.com/demo/results/trainingRGV9BJ-F7K/) ]
---
+++ src/lib/binary.js
@@ -0,0 +1,16 @@
+export const maxBinaryGap = (number) => {
+    const binaryRepresentation = number.toString(2)
+    let zeroCount = 0
+    let maxCount = 0
+
+    for (let digit of binaryRepresentation) {
+        if (digit === '0') {
+            zeroCount += 1
+        } else {
+            maxCount = Math.max(zeroCount, maxCount)
+            zeroCount = 0
+        }
+    }
+
+    return maxCount
+}
