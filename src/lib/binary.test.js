da19b563a840ce355bddcfd1a5e7e0aae08aa02a
Roger Rajaratnam <roger@sourcier.uk>
2020-03-21 15:44:33 +0000

    Add describe blocks to tests

--- src/lib/binary.test.js
+++ src/lib/binary.test.js
@@ -1,25 +1,27 @@
 import { maxBinaryGap } from './binary'
 
-test('9 to have a binary gap of 2', () => {
-    expect(maxBinaryGap(9)).toEqual(2)
-})
+describe('maxBinaryGap', () => {
+    test('9 to have a binary gap of 2', () => {
+        expect(maxBinaryGap(9)).toEqual(2)
+    })
 
-test('20 to have a binary gap of 2', () => {
-    expect(maxBinaryGap(20)).toEqual(1)
-})
+    test('20 to have a binary gap of 2', () => {
+        expect(maxBinaryGap(20)).toEqual(1)
+    })
 
-test('15 to have a binary gap of 0', () => {
-    expect(maxBinaryGap(15)).toEqual(0)
-})
+    test('15 to have a binary gap of 0', () => {
+        expect(maxBinaryGap(15)).toEqual(0)
+    })
 
-test('32 to have a binary gap of 0', () => {
-    expect(maxBinaryGap(32)).toEqual(0)
-})
+    test('32 to have a binary gap of 0', () => {
+        expect(maxBinaryGap(32)).toEqual(0)
+    })
 
-test('529 to have a binary gap of 4', () => {
-    expect(maxBinaryGap(529)).toEqual(4)
-})
+    test('529 to have a binary gap of 4', () => {
+        expect(maxBinaryGap(529)).toEqual(4)
+    })
 
-test('1041 to have a binary gap of 5', () => {
-    expect(maxBinaryGap(1041)).toEqual(5)
+    test('1041 to have a binary gap of 5', () => {
+        expect(maxBinaryGap(1041)).toEqual(5)
+    })
 })
